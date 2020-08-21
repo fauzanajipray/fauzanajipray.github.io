559ca31436895e41fa972964af407486b25e1e90
Fauzan Aji Prayoga <fauzanjr1@gmail.com>
2020-08-21 21:18:36 +0700

    Menyalin isi folder build

---
+++ precache-manifest.5ff95b2883b9beadad1bad5bf62ecd35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0ec7faf2c3bc7c4e103902f99c728b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64cf21dfcc8a072d4ead",
+    "url": "/static/css/main.43aa5471.chunk.css"
+  },
+  {
+    "revision": "aec48ee3108f80e9506b",
+    "url": "/static/js/2.6686dbf0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6686dbf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64cf21dfcc8a072d4ead",
+    "url": "/static/js/main.ceccfa7c.chunk.js"
+  },
+  {
+    "revision": "823694c2996e4c11d31a",
+    "url": "/static/js/runtime-main.d279a6a0.js"
+  }
+]);
